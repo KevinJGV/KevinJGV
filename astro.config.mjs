78c0c475516ec5ca76d8de5f1c7bf21d0a60a5d3
Kevin Gonzalez <soyelsxbbsx@outlook.com>
2024-11-04 22:12:55 -0500

    feat: inicio desarrollo segun wireframe

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://kevinjgv.github.io',
-    base: '/KevinJGV/',
-    outDir: 'docs',
     trailingSlash: 'never'
 });
